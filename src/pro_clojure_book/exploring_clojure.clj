f0756e444a4037fc76816a784bfcc450a274b9d2
Elton Peixoto <elton.peixoto@nubank.com.br>
2023-05-05 16:33:19 -0300

    numbers

--- src/pro_clojure_book/exploring_clojure.clj
+++ src/pro_clojure_book/exploring_clojure.clj
@@ -29,3 +29,41 @@
 ;; there are no statements, only expressions that can be nested in mostly arbitrary ways.
 ;; When evaluated, every expression returns a value
 ;; Miller, Stuart, Breda (2018) p19 Programming Clojure 3th Edition.
+
+
+
+;; Numbers
+(+)    ;=> returns 0
+(+ 2) ;=> returns 2
+
+(- 10 5)
+;=> 5
+(* 3 10 10)
+;=> 300
+(> 5 2)
+;=> true
+(>= 5 5)
+;=> true
+(< 5 2)
+;=> false
+(= 5 2)
+;=> false
+
+
+;Division may surprise you:
+(/ 22 7)
+;=> 22/7
+;As you can see, Clojure has a built-in ratio type.
+;If what you actually want is decimal division, use a floating-point literal for
+;the dividend:
+(/ 22.0 7)
+;=> 3.142857142857143
+
+;=>If you want to stick to integers, you can get the integer quotient and remainder
+;=>with quot and rem:
+(quot 22 7)
+;=> 3
+(rem 22 7)
+;=> 1
+
+
